973f9a687b699be21295aef8ab4cb4adddca5a5e
Berat Çimen <beratcmn@hotmail.com>
2024-07-30 19:21:24 +0300

    feat: Add functionality to create blog directories and files from markdown

--- blog-creator.py
+++ blog-creator.py
@@ -1,13 +1,17 @@
 import os
 import re
 from datetime import datetime
+import markdown2
 
 
-def create_directories(base_path, name):
+def create_directories(base_path, name, raw_blog_html=""):
     now = datetime.now()
     year = now.strftime("%Y")
     month = now.strftime("%m")
     day = now.strftime("%d")
+
+    original_name = name
+    name = to_snake_case(name)
 
     directory_path = os.path.join(base_path, year, month, day, name)
 
@@ -15,12 +19,15 @@ def create_directories(base_path, name):
 
     print(f"Directories created: {directory_path}")
 
-    # Create a HTML file
+    with open("./templates/blog.html", "r", encoding="UTF-8") as f:
+        blog_html = f.read()
+
+    blog_html = blog_html.replace("{{ content }}", raw_blog_html)
+    blog_html = blog_html.replace("{{ title }}", original_name)
+
     file_path = os.path.join(directory_path, "index.html")
-    with open(file_path, "w") as f:
-        f.write(
-            f"<!DOCTYPE html>\n<html>\n<head>\n<title>{name}</title>\n</head>\n<body>\n<h1>{name}</h1>\n</body>\n</html>"
-        )
+    with open(file_path, "w", encoding="UTF-8") as f:
+        f.write(blog_html)
 
     print(f"File created: {file_path}")
 
@@ -53,6 +60,17 @@ def to_snake_case(s):
     return s.lower()
 
 
+def convert_markdown_to_html(file_path):
+    with open(file_path, "r", encoding="UTF-8") as f:
+        content = f.read()
+
+    html = markdown2.markdown(content, extras=["fenced-code-blocks"])
+
+    return html
+
+
 base_path = "./blog"
 name = "Temellere Dönüş, PyTorch ile MNIST"
-create_directories(base_path, to_snake_case(name))
+blog_path = "./blogs/temellere_donus_py_torch_ile_mnist.md"
+blog_html = convert_markdown_to_html(blog_path)
+create_directories(base_path, name, blog_html)
