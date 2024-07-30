ef1ace6e4f36e89028fb7788c81b28649dbd59ff
Berat Çimen <beratcmn@hotmail.com>
2024-07-30 20:10:52 +0300

    feat: Generate blog card HTML in blog-creator.py

--- blog-creator.py
+++ blog-creator.py
@@ -31,6 +31,24 @@ def create_directories(base_path, name, raw_blog_html=""):
 
     print(f"File created: {file_path}")
 
+    print("Generating blog card...")
+
+    with open("./templates/blog-card.html", "r", encoding="UTF-8") as f:
+        blog_card_html = f.read()
+
+    blog_card_html = blog_card_html.replace("{{ title }}", original_name)
+    blog_card_html = blog_card_html.replace("{{ date }}", f"{day}/{month}/{year}")
+    blog_card_html = blog_card_html.replace(
+        "{{ description }}",
+        "PyTorch ile MNIST veri kümesi üzerinde temellere dönüş yaparak, bir sinir ağı modeli oluşturacağız. Bu modeli eğitip, test ederek, modelin doğruluğunu ölçeceğiz.",
+    )
+    blog_card_html = blog_card_html.replace(
+        "{{ route_path }}", directory_path.replace("\\", "/")[1:]
+    )
+
+    print("-" * 50)
+    print(blog_card_html)
+
 
 def to_snake_case(s):
     def replace_turkish_letters(s):
@@ -64,7 +82,15 @@ def convert_markdown_to_html(file_path):
     with open(file_path, "r", encoding="UTF-8") as f:
         content = f.read()
 
-    html = markdown2.markdown(content, extras=["fenced-code-blocks"])
+    html = markdown2.markdown(
+        content,
+        extras=[
+            "fenced-code-blocks",
+            "cuddled-lists",
+            "target-blank-links",
+            "task_list",
+        ],
+    )
 
     return html
 
